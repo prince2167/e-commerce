457ef507b44a9c39141da2e63f647ac47d8799ec
Prince <singhprince8222692@gmail.com>
2023-05-17 14:50:55 +0530

    Add icons

--- src/assets/icon.js
+++ src/assets/icon.js
@@ -2,4 +2,12 @@ import { CiSearch } from "react-icons/ci";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { AiOutlineHeart } from "react-icons/ai";
 import { RiHeartAddLine } from "react-icons/ri";
-export { CiSearch, MdOutlineShoppingCart, AiOutlineHeart, RiHeartAddLine };
+import { BsFillHeartFill } from "react-icons/bs";
+
+export {
+  CiSearch,
+  MdOutlineShoppingCart,
+  AiOutlineHeart,
+  RiHeartAddLine,
+  BsFillHeartFill,
+};
