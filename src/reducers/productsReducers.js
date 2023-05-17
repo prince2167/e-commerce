3c7aced77b141834ddb2f82450bda29904872944
Prince <singhprince8222692@gmail.com>
2023-05-17 15:01:28 +0530

    create: array for wishlist and cart

--- src/reducers/productsReducers.js
+++ src/reducers/productsReducers.js
@@ -1,13 +1,24 @@
 import { products } from "../data/data";
 export const initialState = {
   products,
+  wishlist: [],
+  cart: [],
 };
 
 export const productReducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
-    case value: {
-      return {};
+    case "ADD_TO_WISHLIST": {
+      return {
+        ...state,
+        wishlist: [payload, ...state.wishlist],
+      };
+    }
+    case "REMOVE_FROM_WISHLIST": {
+      return {
+        ...state,
+        wishlist: payload,
+      };
     }
 
     default:
