8f0cbc6b223cf7745feaad028b567fb8e00c1ac9
Prince <singhprince8222692@gmail.com>
2023-05-17 14:53:30 +0530

    feat: wishlist

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,24 +1,62 @@
-import { RiHeartAddLine } from "../../assets/icon";
+import { useNavigate } from "react-router-dom";
+import { RiHeartAddLine, BsFillHeartFill } from "../../assets/icon";
 import classes from "./ProductCard.module.css";
+import { useProducts } from "../../contexts/product-contex";
+import { getIsInWishlist } from "../../utils";
 const ProductsCard = ({ product }) => {
-  const { productName, price, mrp, brand, thumbnail, discountDisplayLabel } =
-    product;
+  const navigate = useNavigate();
+  const { dispatch, state, removeWishlistHandler } = useProducts();
+  const { wishlist } = state;
+  const {
+    productName,
+    price,
+    mrp,
+    brand,
+    thumbnail,
+    discountDisplayLabel,
+    productId,
+  } = product;
+
+  const isInWishlist = getIsInWishlist(wishlist, productId);
+
   return (
-    <div className={classes.cardContainer}>
-      <div className={classes.imgWrapper}>
-        <img src={thumbnail} alt="" />
-        <button className={classes.wishlistButton}>
-          <RiHeartAddLine size="18" />
-        </button>
-      </div>
-      <div className={classes.productDetails}>
-        <strong>{brand}</strong>
-        <p>{productName}</p>
-        <div className={classes.priceDetails}>
-          <p className={classes.price}>₹{price}</p>
-          <p className={classes.mrp}>{mrp}</p>
-          <p className={classes.discountDisplayLabel}>{discountDisplayLabel}</p>
+    <div className={classes.productCard}>
+      <div
+        className={classes.cardContainer}
+        onClick={() => navigate(`/productDetails/${productId}`)}
+      >
+        <img src={thumbnail} alt="" className={classes.img} />
+
+        <div className={classes.productDetails}>
+          <strong>{brand}</strong>
+          <p>{productName}</p>
+          <div className={classes.priceDetails}>
+            <p className={classes.price}>₹{price}</p>
+            <p className={classes.mrp}>{mrp}</p>
+            <p className={classes.discountDisplayLabel}>
+              {discountDisplayLabel}
+            </p>
+          </div>
         </div>
+      </div>
+
+      <div className={classes.wishlistButton}>
+        {isInWishlist ? (
+          <button
+            className={classes.removeWishlistButton}
+            onClick={() => removeWishlistHandler(productId)}
+          >
+            <BsFillHeartFill size="18" />
+          </button>
+        ) : (
+          <button
+            onClick={() =>
+              dispatch({ type: "ADD_TO_WISHLIST", payload: product })
+            }
+          >
+            <RiHeartAddLine size="18" />
+          </button>
+        )}
       </div>
     </div>
   );
