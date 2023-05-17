3b557749955485039ab0369c36424dfeaf6d06a5
Prince <singhprince8222692@gmail.com>
2023-05-17 14:57:33 +0530

    import files & export files

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,6 @@ import Home from "./Home/Home";
 import Cart from "./Cart/Cart";
 import Wishlist from "./Wishlist/Wishlist";
 import LandingPage from "./LandingPage/LandingPage";
-export { Home, Cart, Wishlist, LandingPage };
+import ProductDetails from "./ProductDetails/ProductDetails";
+
+export { Home, Cart, Wishlist, LandingPage, ProductDetails };
