89db9cb80cac4513f61809e0db938a61f1df3dd7
Prince <singhprince8222692@gmail.com>
2023-05-18 15:03:39 +0530

    create helper function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-export const getIsInWishlist = (products, id) => {
+export const getIsInProducts = (products, id) => {
   const product = products?.find((product) => product.productId === Number(id));
 
   return product;
