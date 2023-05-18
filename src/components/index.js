a7dd26232b12c54a24f3ecd09528fa3f852cfd54
Prince <singhprince8222692@gmail.com>
2023-05-18 14:54:10 +0530

    import & export components

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,6 @@ import Navbar from "./Navbar/Navbar";
 import Filter from "./Filter/Filter";
 import Searchbar from "./Searchbar/Searchbar";
 import ProductCard from "./ProductCard/ProductCard";
-export { Navbar, Filter, Searchbar, ProductCard };
+import WishlistCard from "./WishlistCard/WishlistCard";
+import CartCard from "./CartCard/CartCard";
+export { Navbar, Filter, Searchbar, ProductCard, WishlistCard, CartCard };
