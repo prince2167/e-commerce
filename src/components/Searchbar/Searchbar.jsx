33f669f3292236b58aefa4a902d4aa8dd0e4cc85
Prince <singhprince8222692@gmail.com>
2023-05-19 18:39:04 +0530

    feat: seacrh

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import classes from "./Searchbar.module.css";
 import { CiSearch } from "../../assets/icon";
+import { useProducts } from "../../contexts/product-contex";
 const Searchbar = () => {
+  const { dispatch } = useProducts();
   return (
     <div className={classes.group}>
-      <CiSearch className={classes.icon}  />
+      <CiSearch className={classes.icon} />
       <input
         placeholder="Search here"
         type="search"
         className={classes.input}
+        onChange={(event) =>
+          dispatch({ type: "SEARCH_TERM", payload: event.target.value })
+        }
       />
     </div>
   );
