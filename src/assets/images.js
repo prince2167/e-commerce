da916eaa823426898411d686f4cd559ec4627d7b
Prince <singhprince8222692@gmail.com>
2023-05-20 21:08:54 +0530

    Add: images

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,4 +1,9 @@
 const headerImage =
   "https://images-static.nykaa.com/uploads/9322ff16-7f56-413c-a526-0fb2e94df1b5.jpg?tr=w-2400,cm-pad_resize";
 
-  export {headerImage}
+const emptyWishlist =
+  "https://images-static.nykaa.com/nykdesignstudio-images/pub/media/emptyWishlist.jpg";
+
+const emptyCart =
+  "https://asset.nykaafashion.com/static/assets/empty-cart.37b1b704.png";
+export { headerImage, emptyWishlist, emptyCart };
