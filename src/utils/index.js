967a91190e515f71bc9baf302c94a5fd587c2b24
Prince <singhprince8222692@gmail.com>
2023-05-20 21:17:02 +0530

    create: helper function for filter

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,39 @@ export const getIsInProducts = (products, id) => {
 
   return product;
 };
+
+export const getFilteredProducts = (products, filters) => {
+  return filters.length > 0
+    ? products.filter((product) => filters.includes(product.gender))
+    : products;
+};
+
+export const getFilteredByRating = (products, rating) => {
+  const updatedPeoduct =
+    rating.length > 0
+      ? products.filter((product) => product.rating > rating)
+      : products;
+  return updatedPeoduct;
+};
+
+export const getSortedByPrice = (products, sortBy) => {
+  if (sortBy === "Price: Low to High") {
+    products.sort((a, b) => a.price - b.price);
+  }
+  if (sortBy === "Price: High to Low") {
+    products.sort((a, b) => b.price - a.price);
+  }
+  return products;
+};
+
+export const getSortedProductsBySlider = (products, value) => {
+  const updatedProducts = products.filter((product) => product.price > value);
+  return updatedProducts;
+};
+
+export const getSearchProducts = (products, value) => {
+  const updatedProducts = products.filter((product) =>
+    product.productName.toLowerCase().includes(value)
+  );
+  return updatedProducts;
+};
