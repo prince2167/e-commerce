8279f22e69a471c76def46e742b2c0b03cdd7ee4
Prince <singhprince8222692@gmail.com>
2023-05-24 14:18:00 +0530

    export: url

---
+++ src/api/products.js
@@ -0,0 +1,19 @@
+// import axios from "axios";
+
+// export default axios.create({
+//   baseUrl: "http://localhost:3000/",
+// });
+
+// const getProducts = async () => {
+//     const allProducts = await fetchData();
+//     console.log(allProducts);
+//     if (allProducts) {
+//       dispatch({ type: "FETCH_PRODUCTS", payload: allProducts });
+//     }
+//   };
+//   getProducts();
+
+
+const url = "http://localhost:3000/products";
+
+export { url };
