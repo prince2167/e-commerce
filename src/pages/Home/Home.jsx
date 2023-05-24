885baea8848fd7596f88913972ce2474d2446562
Prince <singhprince8222692@gmail.com>
2023-05-24 14:15:11 +0530

    refactor: product listing page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,49 @@
 import { useProducts } from "../../contexts/product-contex";
-import { ProductCard } from "../../components/index";
+import { Filter, ProductCard, Loader } from "../../components/index";
 import classes from "./Home.module.css";
+import {
+  getFilteredByRating,
+  getFilteredProducts,
+  getSearchProducts,
+  getSortedByPrice,
+  getSortedProductsBySlider,
+} from "../../utils";
 const Home = () => {
   const { state } = useProducts();
-  const { products } = state;
+  const {
+    products,
+    categoryFilters,
+    rating,
+    sortBy,
+    priceInput,
+    searchTerm,
+    isLoading,
+  } = state;
+
+  const filteredProducts = getFilteredProducts(products, categoryFilters);
+  const filteredByRatingProducts = getFilteredByRating(
+    filteredProducts,
+    rating
+  );
+  const sortedByPriceProducts = getSortedByPrice(
+    filteredByRatingProducts,
+    sortBy
+  );
+  const sortedProducts = getSortedProductsBySlider(
+    sortedByPriceProducts,
+    priceInput
+  );
+  const searchedProducts = getSearchProducts(sortedProducts, searchTerm);
+
+  if (isLoading) return <Loader />;
   return (
-    <div>
+    <div className={classes.homePage}>
+      <div className={classes.xyz}>
+        <Filter />
+      </div>
+
       <div className={classes.cardList}>
-        {products.map((product) => (
+        {searchedProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
