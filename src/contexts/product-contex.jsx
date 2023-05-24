b7b887320604074ae20164decd40fc048b61d57f
Prince <singhprince8222692@gmail.com>
2023-05-24 14:11:58 +0530

    fetch data from json server

--- src/contexts/product-contex.jsx
+++ src/contexts/product-contex.jsx
@@ -1,5 +1,7 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import { initialState, productReducer } from "../reducers/productsReducers";
+import axios from "axios";
+import { url } from "../api/products";
 const ProductContext = createContext();
 
 const ProductProvider = ({ children }) => {
@@ -11,6 +13,20 @@ const ProductProvider = ({ children }) => {
     );
     dispatch({ type: "REMOVE_FROM_WISHLIST", payload: updatedWishlist });
   };
+
+  const fetchData = async () => {
+    try {
+      dispatch({ type: "ISLOADING_TRUE" });
+      const { data } = await axios.get(url);
+      dispatch({ type: "FETCH_PRODUCTS", payload: data });
+      dispatch({ type: "ISLOADING_FALSE" });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <ProductContext.Provider value={{ state, dispatch, removeWishlistHandler }}>
       {children}
