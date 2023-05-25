b952de05b3af7c80af87401444d792991d26ce3d
Prince <singhprince8222692@gmail.com>
2023-05-25 17:36:42 +0530

    Add: authProvider to main file

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { ProductProvider } from "./contexts/product-contex.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth-context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ProductProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ProductProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <ProductProvider>
+          <App />
+        </ProductProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
