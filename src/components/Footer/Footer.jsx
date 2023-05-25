eedcc92fb1851f902ac4ee26a93e319c6e8dc260
Prince <singhprince8222692@gmail.com>
2023-05-25 17:37:39 +0530

    Create: footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return <div className={classes.footerContainer}>
+    <h3>© 2023 MyShoopingSite.com</h3>
+  </div>;
+};
+
+export default Footer;
