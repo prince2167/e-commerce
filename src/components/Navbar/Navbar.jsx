64e259eb1bd50350f54ab244ef7f3bb306421bdd
Prince <singhprince8222692@gmail.com>
2023-05-25 17:35:26 +0530

    refactor: wishlist and cart functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,30 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate, useNavigation } from "react-router-dom";
 import classes from "./Navbar.module.css";
 import { Searchbar } from "../index";
 import {
   MdOutlineShoppingCart,
   AiOutlineHeart,
   RiAccountCircleLine,
+  RiAccountCircleFill,
 } from "../../assets/icon";
 import { useProducts } from "../../contexts/product-contex";
+import { useAuth } from "../../contexts/auth-context";
+
 const Navbar = () => {
+  const { authState, logout } = useAuth();
+  const { user } = authState;
   const { state } = useProducts();
   const { cart, wishlist } = state;
   const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <nav>
       <div className={classes.navbar}>
@@ -22,30 +36,52 @@ const Navbar = () => {
         </div>
 
         <div className={classes.navigator}>
-          <button
-            className={classes.loginButton}
-            onClick={() => navigate("/login")}
-          >
-            Login
-          </button>
+          {user ? (
+            <button className={classes.loginButton} onClick={handleLogout}>
+              Logout
+            </button>
+          ) : (
+            <button
+              className={classes.loginButton}
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </button>
+          )}
 
-          <div className={classes.wishlistNavlink}>
-            <NavLink to="wishlist">
-              <AiOutlineHeart size="23" />
-            </NavLink>
-            {wishlist.length > 0 && (
-              <p className={classes.wishlistLength}>{wishlist.length}</p>
-            )}
-          </div>
+          {user ? (
+            <div className={classes.wishlistNavlink}>
+              <NavLink to="/wishlist">
+                <AiOutlineHeart size="23" />
+              </NavLink>
+              {wishlist.length > 0 && (
+                <p className={classes.wishlistLength}>{wishlist.length}</p>
+              )}
+            </div>
+          ) : (
+            <div className={classes.wishlistNavlink}>
+              <NavLink to="/login">
+                <AiOutlineHeart size="23" />
+              </NavLink>
+            </div>
+          )}
 
-          <div className={classes.cartNavlink}>
-            <NavLink to="/cart">
-              <MdOutlineShoppingCart size="23" />
-            </NavLink>
-            {cart.length > 0 && (
-              <p className={classes.cartLength}>{cart.length}</p>
-            )}
-          </div>
+          {user ? (
+            <div className={classes.cartNavlink}>
+              <NavLink to="/cart">
+                <MdOutlineShoppingCart size="23" />
+              </NavLink>
+              {cart.length > 0 && (
+                <p className={classes.cartLength}>{cart.length}</p>
+              )}
+            </div>
+          ) : (
+            <div className={classes.cartNavlink}>
+              <NavLink to="/login">
+                <MdOutlineShoppingCart size="23" />
+              </NavLink>
+            </div>
+          )}
         </div>
       </div>
 
@@ -56,29 +92,52 @@ const Navbar = () => {
           </NavLink>
 
           <div className={classes.navigator}>
-            <NavLink to="/login">
-              <RiAccountCircleLine size="23" />
-            </NavLink>
-
-            <div className={classes.wishlistNavlink}>
-              <NavLink to="wishlist">
-                <AiOutlineHeart size="23" />
+            {user ? (
+              <NavLink onClick={handleLogout}>
+                <RiAccountCircleFill size="23" />
               </NavLink>
-              {wishlist.length > 0 && (
-                <p className={classes.wishlistLength}>{wishlist.length}</p>
-              )}
-            </div>
-
-            <div className={classes.cartNavlink}>
-              <NavLink to="/cart">
-                <MdOutlineShoppingCart size="23" />
+            ) : (
+              <NavLink to="/login">
+                <RiAccountCircleLine size="23" />
               </NavLink>
-              {cart.length > 0 && (
-                <p className={classes.cartLength}>{cart.length}</p>
-              )}
-            </div>
+            )}
+
+            {user ? (
+              <div className={classes.wishlistNavlink}>
+                <NavLink to="wishlist">
+                  <AiOutlineHeart size="23" />
+                </NavLink>
+                {wishlist.length > 0 && (
+                  <p className={classes.wishlistLength}>{wishlist.length}</p>
+                )}
+              </div>
+            ) : (
+              <div className={classes.wishlistNavlink}>
+                <NavLink to="/login">
+                  <AiOutlineHeart size="23" />
+                </NavLink>
+              </div>
+            )}
+
+            {user ? (
+              <div className={classes.cartNavlink}>
+                <NavLink to="/cart">
+                  <MdOutlineShoppingCart size="23" />
+                </NavLink>
+                {cart.length > 0 && (
+                  <p className={classes.cartLength}>{cart.length}</p>
+                )}
+              </div>
+            ) : (
+              <div className={classes.cartNavlink}>
+                <NavLink to="/login">
+                  <MdOutlineShoppingCart size="23" />
+                </NavLink>
+              </div>
+            )}
           </div>
         </div>
+        
         <div className={classes.searchbar}>
           <Searchbar />
         </div>
