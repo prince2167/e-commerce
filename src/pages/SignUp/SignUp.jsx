565e85d80b20926a01f44d16b742e57b941f7c6f
Prince <singhprince8222692@gmail.com>
2023-05-28 19:08:21 +0530

    create: sign up card

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,59 +1,119 @@
+import GoogleButton from "react-google-button";
 import classes from "./SignUp.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/auth-context";
 const SignUp = () => {
+  const { authDispatch, authState, signUp, googleSignin } = useAuth();
+  const { email, password, error, confirmPassword } = authState;
+  const navigate = useNavigate();
+  const singUpHandler = async (event) => {
+    event.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate("/login");
+    } catch (error) {
+      authDispatch({ type: "ERROR", payload: error.message });
+    }
+  };
+
+  const googleSignupHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      if (password === confirmPassword) {
+        await googleSignin();
+        navigate("/home");
+      } else {
+        alert("Password does not match");
+      }
+    } catch (error) {
+      authDispatch({ type: "ERROR", payload: error.message });
+    }
+  };
   return (
     <div className={classes.formContainer}>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={singUpHandler}>
         <p className={classes.title}>Register </p>
         <p className={classes.message}>
           Signup now and get full access to our app.{" "}
         </p>
+        {error && (
+          <p variant="danger" className={classes.errorAlert}>
+            {error}
+          </p>
+        )}
         <div className={classes.flex}>
           <label>
             <input
-              required=""
+              required
               placeholder="First Name"
               type="text"
-              class={classes.input}
+              className={classes.input}
+              onChange={(event) =>
+                authDispatch({
+                  type: "FIRST_NAME",
+                  payload: event.target.value,
+                })
+              }
             />
           </label>
 
           <label>
             <input
-              required=""
+              required
               placeholder="Last Name"
               type="text"
-              class={classes.input}
+              className={classes.input}
+              onChange={(event) =>
+                authDispatch({ type: "LAST_NAME", payload: event.target.value })
+              }
             />
           </label>
         </div>
         <label>
           <input
-            required=""
+            required
             placeholder="e-mail"
             type="text"
-            class={classes.input}
+            className={classes.input}
+            onChange={(event) =>
+              authDispatch({ type: "E_MAIL", payload: event.target.value })
+            }
           />
         </label>
 
         <label>
           <input
-            required=""
+            required
             placeholder="password"
-            type="text"
-            class={classes.input}
+            type="password"
+            className={classes.input}
+            onChange={(event) =>
+              authDispatch({ type: "PASSWORD", payload: event.target.value })
+            }
           />
         </label>
 
         <label>
           <input
-            required=""
+            required
             placeholder="confirm-password"
-            type="text"
-            class={classes.input}
+            type="password"
+            className={classes.input}
+            onChange={(event) =>
+              authDispatch({
+                type: "CONFIRM_PASSWORD",
+                payload: event.target.value,
+              })
+            }
           />
         </label>
-        <button className={classes.submit}>Sign up</button>
+        <button type="submit" className={classes.submit}>
+          Sign up
+        </button>
+        <button type="submit" className={classes.googleButton}>
+          <GoogleButton type="light" onClick={googleSignupHandler} />
+        </button>
         <p className={classes.signin}>
           Already have an Account? <Link to="/login"> Sign in</Link>
         </p>
