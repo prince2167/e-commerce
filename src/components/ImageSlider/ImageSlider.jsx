d1f63255f95f32eb9d3b8c58a5089f7acfff98b2
Prince <singhprince8222692@gmail.com>
2023-05-28 18:51:30 +0530

    image slider for multiple images

---
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,32 @@
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "../../assets/icon";
+import { useProducts } from "../../contexts/product-contex";
+import classes from "./ImageSlider.module.css";
+
+const ImageSlider = ({ images }) => {
+  const { state, dispatch } = useProducts();
+  const { currentIndex } = state;
+
+  return (
+    <div className={classes.images}>
+      <img src={images[currentIndex].url} />
+      <AiOutlineArrowLeft
+        size="40"
+        className={
+          currentIndex === 0 ? classes.disabledLeftArrow : classes.leftArrow
+        }
+        onClick={() => dispatch({ type: "PREVIOUS_CURRENT_INDEX" })}
+      />
+      <AiOutlineArrowRight
+        size="40"
+        className={
+          currentIndex === images.length - 1
+            ? classes.disableRigthArrow
+            : classes.rigthArrow
+        }
+        onClick={() => dispatch({ type: "NEXT_CURRENT_INDEX" })}
+      />
+    </div>
+  );
+};
+
+export default ImageSlider;
