ea1edf98774a3cf178b00374b34ce52be5cd8ee5
Prince <singhprince8222692@gmail.com>
2023-05-28 18:58:16 +0530

    reafactor: data for raitings

--- src/data/filterData.js
+++ src/data/filterData.js
@@ -1,5 +1,5 @@
 export const category = ["Men", "Women"];
 
-export const ratings = [4, 3, 2, 1];
+export const ratingsBy = ["4", "3", "2", "1"];
 
 export const sortingBy = ["Price: Low to High", "Price: High to Low"];
