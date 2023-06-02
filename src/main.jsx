bf609db58d6dbc0f06ae0c3a1f27d1b104aca132
Prince <singhprince8222692@gmail.com>
2023-06-02 22:25:17 +0530

    add: toastcontainer

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,19 @@ import "./index.css";
 import { ProductProvider } from "./contexts/product-contex.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth-context.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
+        <ToastContainer
+          position="bottom-left"
+          pauseOnHover={false}
+          theme="colored"
+          draggable={true}
+        />
         <ProductProvider>
           <App />
         </ProductProvider>
