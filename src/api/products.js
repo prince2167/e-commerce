5572aeaa898e434ece5a83d8bdedfe8508ff74de
Prince <singhprince8222692@gmail.com>
2023-06-12 18:16:40 +0530

    add env variable as url

--- src/api/products.js
+++ src/api/products.js
@@ -1,19 +1,2 @@
-// import axios from "axios";
-
-// export default axios.create({
-//   baseUrl: "http://localhost:3000/",
-// });
-
-// const getProducts = async () => {
-//     const allProducts = await fetchData();
-//     console.log(allProducts);
-//     if (allProducts) {
-//       dispatch({ type: "FETCH_PRODUCTS", payload: allProducts });
-//     }
-//   };
-//   getProducts();
-
-
-const url = "http://localhost:3000/products";
-
+const url = import.meta.env.VITE_APP_SERVER_URL;
 export { url };
